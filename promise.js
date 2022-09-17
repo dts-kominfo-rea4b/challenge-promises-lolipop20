b832e18337ed0339dda18ca10ab040b3aa0fe326
lolipo20 <adli.nur11@gmail.com>
2022-09-17 20:28:16 +0700

    modify promise.js

--- promise.js
+++ promise.js
@@ -1,7 +1,15 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+const promiseOutput = async (params) => {
+  let a = await promiseTheaterIXX();
+  let b = await promiseTheaterVGC();
+
+  return new Promise((resolve) =>{
+    const hasil = [...a,...b]?.filter(res => res.hasil === params)?.length
+    return resolve(hasil);
+  });
+}
 
 module.exports = {
   promiseOutput,
